f3712eb7843cf1800e7f7408f0d7f2a872a5f97b
mine2424 <minewest4320@outlook.jp>
2021-09-19 13:59:33 +0900

    feat: firebase emulatorの設定の分岐

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -18,6 +23,11 @@ Future<void> main() async {
 Future<void> _initFirebase() async {
   await Firebase.initializeApp();
 
+  const fbEmuHost = String.fromEnvironment('FB_EMU_HOST');
+  if (fbEmuHost.isNotEmpty) {
+    await _useFirebaseEmulator();
+  }
+
   final remoteConfig = RemoteConfig.instance;
 
   await remoteConfig.setConfigSettings(RemoteConfigSettings(
@@ -26,4 +36,15 @@ Future<void> _initFirebase() async {
   ));
 
   await AppService.prepare();
+}
+
+Future<void> _useFirebaseEmulator() async {
+  final localHostString = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+  await FirebaseAuth.instance.useEmulator('http://$localHostString:9099');
+  FirebaseFirestore.instance.settings = Settings(
+    host: '$localHostString:8080',
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+  // await FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
 }
